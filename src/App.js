c221484d4fe05e293e0b89aa2df6d71f56e76a5d
rodney-e-harris <90345548+rodney-e-harris@users.noreply.github.com>
2024-02-05 15:17:09 -0600

    Modified index.js, landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      {/* Temporary Navbar untill CSS added  */}
+      <header>
+        <ul>
+          <li><a href="/">About</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="works-in-progress">Works-In-Progress</a></li>
+        </ul>
       </header>
+
+    <h1>Rodney Harris</h1>
+
+      {/* -- Picture */}
+      <img src={require("./assets/ProfilePic.jpg")}>
+      </img>
+
+      {/* -- About Me  */}
+      <p>
+        This would be the section for an explanation of the lab and the broader application areas.
+      </p>
+
+      {/* -- The footer will contain sections where a user can find links to Github, relevant social medias and contact information */}
+      <h3>Footer Placeholders</h3>
+
+      <footer>
+        <ul>
+          <li><a href="https://github.com/rodney-e-harris" target="_blank" rel="noopener">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/rodney-harris-66840a199/" target="_blank" rel="noopener">LinkedIn</a></li>
+          <li><a href="https://google.com" target="_blank" rel="noopener">Contact Information</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
